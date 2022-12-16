835caa41c7f90217342c66890f76fd01550efe9d
raphael-fournier <raphaelfournier1123@gmail.com>
2022-12-16 02:14:28 +0100

    add registration numbers to existing users

--- backend/migrations/20221215130159-raphael_insert_test_data.js
+++ backend/migrations/20221215130159-raphael_insert_test_data.js
@@ -9,15 +9,29 @@ module.exports = {
     );
     await q.sequelize.query(
       `
+      UPDATE users
+      SET registration_number = '12345'
+      WHERE first_name = 'John' and last_name = 'Doe'
+      `
+    );
+    await q.sequelize.query(
+      `
+      UPDATE users
+      SET registration_number = '123456'
+      WHERE first_name = 'Jean' and last_name = 'Dupont'
+      `
+    );
+    await q.sequelize.query(
+      `
       INSERT INTO users (id, phone_number, first_name, last_name, last_connected, eventId, registration_number)
-        SELECT UUID(), '+33699999999', 'Louis', 'Dubois', NULL, id, '123456'
+        SELECT UUID(), '+33699999999', 'Louis', 'Dubois', NULL, id, '1234567'
         FROM events WHERE name='Example Event 3';
       `
     );
     await q.sequelize.query(
       `
       INSERT INTO users (id, phone_number, first_name, last_name, last_connected, eventId, registration_number)
-        SELECT UUID(), '+33611223344', 'Louise', 'Morel', NULL, id, '112233'
+        SELECT UUID(), '+33611223344', 'Louise', 'Morel', NULL, id, '12345678'
         FROM events WHERE name='Example Event 4';
       `
     );
